e8b13dc54e8d2b6e0ea91d6bf4e49a2f60247500
Prashant Singh <41435180+prashant1k99@users.noreply.github.com>
2021-03-24 12:31:27 +0530

    fixed email validation

--- src/utils/validations/index.js
+++ src/utils/validations/index.js
@@ -1,9 +1,6 @@
 const emailValidator = (email) => {
   const re = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,7})+$/
-  if (re.test(email) === true) {
-    return true
-  }
-  return re.test(email) || false
+  return re.test(email)
 }
 
 export { emailValidator }
